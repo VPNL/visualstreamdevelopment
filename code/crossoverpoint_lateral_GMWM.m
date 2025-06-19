d329e075c8fc04dee23dc8af1d37f9fb4924233f
Karla Perez <Karlap15@stanford.edu>
2025-06-19 08:54:11 -0700

    Update crossoverpoint_lateral_GMWM.m

--- code/crossoverpoint_lateral_GMWM.m
+++ code/crossoverpoint_lateral_GMWM.m
@@ -23,7 +23,7 @@ forgroup=[]; group=[];
 [c1 c2 group] =unique(forgroup);
 
 %% Define colors schemes and stream names
-streamcolor{1} = [[85 161 247]/255; [36 134 244]/255; [11 108 219]/255; [8 84 170]/255; [6 60 122]/255] % lateral (blue) color scheme
+streamcolor{1} = [[85 161 247]/255; [36 134 244]/255; [11 108 219]/255; [8 84 170]/255; [6 60 122]/255] % color scheme for lateral stream (blue tones)
 streamcolor{2} = [[80 80 80]/255;[120 120 120]/255; [160 160 160]/255]; % eva color scheme
 
 streamname{1} = 'lateral'
