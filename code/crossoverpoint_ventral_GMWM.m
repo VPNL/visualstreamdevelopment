e8badf61acbdd49ce85910bd0f63581ba47067c2
Karla Perez <Karlap15@stanford.edu>
2025-06-19 09:01:17 -0700

    Update crossoverpoint_ventral_GMWM.m

--- code/crossoverpoint_ventral_GMWM.m
+++ code/crossoverpoint_ventral_GMWM.m
@@ -48,7 +48,7 @@ figure; set(gcf,'color','white'); hold;
 FULVAL_GM=str_GM(2).All_R1;
 FULVAL_WM=str_WM(2).All_R1;
 
-%% Early visual gray matter 
+%% Early visual areas gray matter 
 for roi=1:length(str_GM(2).roi_list)
     VAL= FULVAL_GM(:,roi);
     
@@ -74,7 +74,7 @@ for roi=1:length(str_GM(2).roi_list)
     ylabel({'R1 [1/s]'});  xlabel({'Age in days'});
     
     
-    %% Early visual white matter 
+    %% Early visual areas white matter 
     VAL= FULVAL_WM(:,roi);
     tbl= table(age', double(VAL), group,'VariableNames',{'Age','meanR1','Baby'});
     lme1= fitlme(tbl,'meanR1 ~ Age + (1|Baby)'); %% fitlme :  this is a matlab function to run LMM
